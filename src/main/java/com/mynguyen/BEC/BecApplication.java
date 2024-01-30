a29fb29f0ab1ee90bb0f5a4241cfbe089a8d8c97
my-nguyen <my.nguyen@navercorp.com>
2024-01-30 17:27:46 +0700

    init

---
+++ src/main/java/com/mynguyen/BEC/BecApplication.java
@@ -0,0 +1,13 @@
+package com.mynguyen.BEC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mynguyen/BEC/BecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mynguyen.BEC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
